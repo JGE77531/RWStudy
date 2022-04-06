7dcec43340d80401198f8981b026f8d8c4ab73cc
zzz77531 <wjsrkdms77531@gmail.com>
2022-04-06 16:50:41 +0900

    [ES] 6강업로드

---
+++ 0406/hanoi.js
@@ -0,0 +1,6 @@
+var btn_r = document.getElementById("button_reset");
+
+function init(){
+    var f = document.getElementsByClassName("first")[0];
+    f.innerHTML = "🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥<br>🟨🟨🟨🟨🟨🟨🟨🟨🟨<br>🟩🟩🟩🟩🟩🟩🟩🟩<br>🟦🟦🟦🟦🟦<br>🟪🟪🟪<br>⬛";
+}
